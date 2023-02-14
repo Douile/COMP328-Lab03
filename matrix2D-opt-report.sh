3138c8b073c4805dc32099faa0147fa894b2672a
Douile <25043847+Douile@users.noreply.github.com>
2023-02-14 23:16:16 +0000

    Rename opt report files

--- matrix2D-opt-report.sh
+++ matrix2D-opt-report.sh
@@ -7,5 +7,7 @@ FILE="${1:-matrix2D.c}"
 BASE_FILE="$(basename "$FILE" ".c")"
 
 for level in $(seq 0 3); do
-  $CC "-O$level" "$FILE" -o "$BASE_FILE-o$level" -qopt-report3
+  output="$BASE_FILE-o$level"
+  $CC "-O$level" "$FILE" -o "$output" -qopt-report3
+  mv "$BASE_FILE.optrpt" "$output.optrpt"
 done
