ce5e042692925e3a9d0b5e02f8edd9c7cf214000
Douile <25043847+Douile@users.noreply.github.com>
2023-02-14 19:20:32 +0000

    memory_access: Rename variables

--- memory_access.c
+++ memory_access.c
@@ -1,31 +1,35 @@
-
 #include <stdio.h>
 #include <time.h>
-int N = 99999;
-int mat[99999][99999];
+
+#define SIZE 99999
+int matrices[SIZE][SIZE];
 
 void main()
-
 {
+    // Counters
     int i, j;
-    clock_t _S, _E;
+    // Timings
+    clock_t start, end;
     double d = 0.0;
 
-    _S = clock();
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-            mat[i][j] = mat[i][j] + (mat[i][j] * mat[i][j]);
+    start = clock();
+    // Iterate on rows
+    for (i = 0; i < SIZE; i++)
+        for (j = 0; j < SIZE; j++)
+            matrices[i][j] = matrices[i][j] + (matrices[i][j] * matrices[i][j]);
 
-    _E = clock();
-    d = (double)(_E - _S) / CLOCKS_PER_SEC;
+    // Output timings
+    end = clock();
+    d = (double)(end - start) / CLOCKS_PER_SEC;
     printf("row major run time is %lf milliseconds\n", 1000 * d);
 
-    _S = clock();
-    for (j = 0; j < N; j++)
-        for (i = 0; i < N; i++)
-            mat[i][j] = mat[i][j] + (mat[i][j] * mat[i][j]);
+    start = clock();
+    // Iterate on columns
+    for (j = 0; j < SIZE; j++)
+        for (i = 0; i < SIZE; i++)
+            matrices[i][j] = matrices[i][j] + (matrices[i][j] * matrices[i][j]);
 
-    _E = clock();
-    d = (double)(_E - _S) / CLOCKS_PER_SEC;
+    end = clock();
+    d = (double)(end - start) / CLOCKS_PER_SEC;
     printf("column major run time is %lf milliseconds\n", 1000 * d);
 }
